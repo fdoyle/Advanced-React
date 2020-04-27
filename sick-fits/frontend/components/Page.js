5444f0342da2c02e9552d7983b2fbc3697349729
Frank Doyle <fdoyl001@gmail.com>
2020-04-26 22:02:56 -0500

    finish #6 and #7 themes and global styling with styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 const theme = {
   red: "#FF0000",
@@ -13,13 +13,48 @@ const theme = {
   bs: "0 12px 24px 0 rgba(0,0,0,.09)",
 };
 
+theme.lightgrey = theme.lightGrey; // account for typo in provided code
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url("/static/radnikanext-medium-webfont.woff2")
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+
+  }
+
+  html{
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 const StyledPage = styled.div`
   background: white;
-  color: ${props => props.theme.black};
+  color: ${(props) => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: ${props => props.theme.maxWidth};
+  max-width: ${(props) => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
